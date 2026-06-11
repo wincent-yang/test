c877c15ba865f02a6e2cc004ea6a3a012ac805b8
yangwenkai <2921264557@qq.com>
2026-06-11 16:54:48 +0800

    新增 URL.md 文件，介绍 URL 及其编码规则

--- DIY/1.py
+++ DIY/1.py
@@ -1,2 +1,28 @@
+#help(print)
 
-print(int("0xff",16)) # 255
+class Animal:
+    def eat(self, food: str) -> None:
+        print(f"进食：{food}")
+
+a = Animal()
+a.eat("米饭")   # 正常
+a.eat(100)   # 报错/IDE警告：类型不匹配
+
+# 1. 先定义食物类
+class Food:
+    def __init__(self, name: str):
+        self.name = name
+
+# 2. 动物类，限定参数必须是 Food 实例
+class Animal:
+    def eat(self, food: Food) -> None:
+        print(f"正在吃 {food.name}")
+
+# 使用
+rice = Food("米饭")
+cat = Animal()
+cat.eat(rice)  # 正常
+
+cat.eat("米饭")  # 报错：不是 Food 对象
+
+
